b3cdaefcd0179ad56a78bc32784a97b48fc90e31
SAJJAD941994 <sajjad94.1994@gmail.com>
2016-04-15 19:39:44 +0300

    Create dev.lua

---
+++ plugins/dev.lua
@@ -0,0 +1,24 @@
+do
+
+function run(msg, matches)
+return [[
+
+االبوت الذي يعمل على مجوعات السوبر 🔸
+
+يعمل البوت على مجموعات سوبر تصل الى5k عضو 🔷
+
+     ≪تم صنع البوت بواسطة المطور≫
+                      『 @SAJJADNOORI 』
+            🔹#Dev #SAJJADNOORI🔹
+]]
+end
+
+return {
+description = "Shows bot q", 
+usage = "spam Shows bot q",
+patterns = {
+"/dev$"
+},
+run = run 
+}
+end

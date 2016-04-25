872afefb4b1f5cffa1d7460f2316b3473472266a
ASD_KARBALA <sajjad94.1994@gmail.com>
2016-04-25 21:11:51 +0300

    Create ar-onservice.lua

---
+++ plugins/ar-onservice.lua
@@ -0,0 +1,32 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY SAJJAD NOORI                   ▀▄ ▄▀ 
+▀▄ ▄▀     BY SAJAD NOORI (@SAJJADNOORI)    ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY SAJJAD NOORI          ▀▄ ▄▀   
+▀▄ ▄▀     KICK BOT  : طرد البوت            ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+do
+
+local function run(msg, matches)
+local bot_id = our_id 
+local receiver = get_receiver(msg)
+    if matches[1] == 'طرد البوت' and is_admin1(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+	   leave_channel(receiver, ok_cb, false)
+    elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
+       send_large_msg(receiver, 'This is not one of my groups.', ok_cb, false)
+       chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
+	   leave_channel(receiver, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^(طرد البوت)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end

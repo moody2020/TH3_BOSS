ff4d95c1cc9bdee8c7f79c3f5c40f62792c3ee7b
ASD_KARBALA <sajjad94.1994@gmail.com>
2016-04-25 21:47:18 +0300

    Update en-insta.lua

--- plugins/en-insta.lua
+++ plugins/en-insta.lua
@@ -48,7 +48,7 @@ local function instagramUser(msg, query)
 	if user.data.website ~= '' then
 		text = text.."❣ Website : "..user.data.website.."\n"
 	end
-	text = text.."\n❣ #Dev @SAJJADNOORI"
+	text = text.."\n❣ #Dev @SAJJADNOORI\n❣ #Bot_channel : @S94IQ"
 	local file_path = download_to_file(user.data.profile_picture,"insta.png")     -- disable this line if you want to send profile photo as sticker
 	--local file_path = download_to_file(user.data.profile_picture,"insta.webp")    -- enable this line if you want to send profile photo as sticker
 	local cb_extra = {file_path=file_path}
@@ -82,7 +82,7 @@ local function instagramMedia(msg, query)
 		  text = text.."❣ Name : "..jdat.data.user.full_name.."\n"
 		  text = text.."❣ Like Count : "..jdat.data.likes.count.."\n"
 	end
-	text = text.."\n❣ #Dev @SAJJADNOORI"
+	text = text.."\n❣ #Dev @SAJJADNOORI\n❣ #Bot_channel : @S94IQ"
 	send_msg(receiver,text,ok_cb,false)
 end
 local function run(msg, matches)

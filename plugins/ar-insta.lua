54aca0ae6b4f6aba8e06594ce4e852c658a8892f
ASD_KARBALA <sajjad94.1994@gmail.com>
2016-04-25 21:14:57 +0300

    Update ar-insta.lua

--- plugins/ar-insta.lua
+++ plugins/ar-insta.lua
@@ -48,7 +48,7 @@ local function instagramUser(msg, query)
 	if user.data.website ~= '' then
 		text = text.."❣ الموقع : "..user.data.website.."\n"
 	end
-	text = text.."\n❣ #المطور @SAJJADNOORI"
+	text = text.."\n❣ #المطور @SAJJADNOORI\n❣ #قناة_البوت : @S94IQ"
 	local file_path = download_to_file(user.data.profile_picture,"insta.png")     -- disable this line if you want to send profile photo as sticker
 	--local file_path = download_to_file(user.data.profile_picture,"insta.webp")    -- enable this line if you want to send profile photo as sticker
 	local cb_extra = {file_path=file_path}
@@ -82,7 +82,7 @@ local function instagramMedia(msg, query)
 		  text = text.."❣ الاسم : "..jdat.data.user.full_name.."\n"
 		  text = text.."❣ عدد الايك: "..jdat.data.likes.count.."\n"
 	end
-	text = text.."\n@SAJJADNOORI"
+	text = text.."\n@SAJJADNOORI\n❣ #قناة_البوت : @S94IQ"
 	send_msg(receiver,text,ok_cb,false)
 end
 local function run(msg, matches)

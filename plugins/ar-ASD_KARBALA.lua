dfcfe007323531481f64aa735c32f4c96a8724ab
ASD_KARBALA <sajjad94.1994@gmail.com>
2016-05-17 06:04:57 +0300

    Update ar-ASD_KARBALA.lua

--- plugins/ar-ASD_KARBALA.lua
+++ plugins/ar-ASD_KARBALA.lua
@@ -11,12 +11,14 @@
 do
 
 function run(msg, matches)
-  return '❣  السورس 🛢 ASD_KARBALA 📁\n❣  الاصدار 📋 v1.2\n❣ الموقع 💻\n \nhttps://github.com/SAJJAD94/ASD_KARBALA.git \n❣   #المطور : @SAJJADNOORI \n❣ #بوت_المطور : @S94_BOT \n ❣ #قناة_البوت : @S94IQ'
+local reply_id = msg['id']
+  local S = '❣  السورس 🛢 ASD_KARBALA 📁\n❣  الاصدار 📋 v1.2\n❣ الموقع 💻\n \nhttps://github.com/SAJJAD94/ASD_KARBALA.git \n❣   #المطور : @SAJJADNOORI \n❣ #بوت_المطور : @S94_BOT \n ❣ #قناة_البوت : @S94IQ'
+reply_msg(reply_id, S, ok_cb, false)
 end
 
 return {
   patterns = {
-    "^الاصدار"
+"^(الاصدار)$",
   }, 
   run = run 
 }

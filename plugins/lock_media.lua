7b85e1986dac9164f7f31990e12b263a336d79cc
moody2020 <mrmohammed98@mail.ru>
2016-06-20 18:50:10 +0300

    Update lock_media.lua

--- plugins/lock_media.lua
+++ plugins/lock_media.lua
@@ -1,46 +1,60 @@
-local function kick_user(user_id, chat_id)
-local chat = 'chat#id'..chat_id
-local user = 'user#id'..user_id
-chat_del_user(chat, user, function (data, success, result)
-if success ~= 1 then
-send_msg(data.chat, 'Error while kicking user', ok_cb, nil)
-end
-end, {chat=chat, user=user})
-end
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY MOHAMMED HISHAM                ▀▄ ▄▀ 
+▀▄ ▄▀   BY MOHAMMEDHISHAM (@oOMOOMOo)      ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM       ▀▄ ▄▀   
+▀▄ ▄▀                   كتم الوسائط            ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+do 
 
-local function run (msg, matches)
-local user = msg.from.id
-local chat = msg.to.id
+local function pre_process(msg) 
+local jalal = msg['id'] 
+  local user = msg.from.id 
+local chat = msg.to.id 
+    local oscar = 'mate:'..msg.to.id 
+    if redis:get(oscar) and msg.media and not is_momod(msg) then 
 
-if msg.to.type ~= 'chat' then
-return "ممنوع ⛔️نشر الروابط + الاعلانات  +الروابط +الصور + اعاد توجيه + الكروبات + القنوات"..'📝Name : '..msg.from.first_name..'\n'..'🔅يوزرك : @'..msg.from.username..'\n'
-elseif user == tostring(our_id) then
---[[A robot must protect its own existence as long as such protection does
-not conflict with the First or Second Laws.]]--
-return ""
-elseif is_sudo(msg) then
-return "😕"
-else
-kick_user(user, chat)
-end
-end
+            delete_msg(msg.id, ok_cb, false) 
+local test = "عزيزي~["..msg.from.first_name.."]".."\n".."يمنع نشر صور فيديوهات صوتيات وكافة الميديا هنا ان تكرر الامر سوف تجبرني على طردك🙂☝️".."\n".."#username: @"..(msg.from.username or " ") 
+reply_msg(jalal, test, ok_cb, true) 
 
-return {
-description = "Bot kicks user",
-usage = {
-"!kickme"
-},
-patterns = {
-"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
-"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
-"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
-"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
-"[Hh][Tt][Tt][Pp]://",
-"[Ww][Ww][Ww]",
-".[Ii][Rr]",
-".[Cc][Oo][Mm]",
-"[Hh][Tt][Tt][Pp]://",
-"[Hh][Tt][Tt][Pp][Ss]://"
-},
-run = run,
-}
+end 
+
+        return msg 
+    end 
+
+local function run(msg, matches) 
+local jalal = msg['id'] 
+
+    if matches[1] == 'كتم الوسائط'  and is_momod(msg) then 
+                    local oscar = 'mate:'..msg.to.id 
+                    redis:set(oscar, true) 
+                    local oscar1 = ' تم كتم جميع الوسائط🔕' 
+reply_msg(jalal, oscar1, ok_cb, true) 
+elseif matches[1] == 'كتم الوسائط' and not is_momod(msg) then 
+local asdy = 'للمشرفين فقط🔴' 
+reply_msg(jalal, asdy, ok_cb, true) 
+
+elseif matches[1] == 'الغاء كتم الوسائط' and not is_momod(msg) then 
+      local oscar = 'mate:'..msg.to.id 
+      redis:del(oscar) 
+    local don = ' تم الغاء كتم الوسائط🔔' 
+reply_msg(jalal, don, ok_cb, true) 
+elseif matches[1] == 'الغاء كتم الوسائط' and not is_momod(msg) then 
+local jalal_aldon = 'للمشرفين فقط🔴' 
+reply_msg(jalal, jalal_aldon, ok_cb, true) 
+end 
+end 
+
+return { 
+    patterns = { 
+    "^(كتم الوسائط)$", 
+    "^(الغاء كتم الوسائط)$" 
+    }, 
+run = run, 
+    pre_process = pre_process 
+} 
+
+end

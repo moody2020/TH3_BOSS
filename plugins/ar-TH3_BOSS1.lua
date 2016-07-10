766aabdd0c03858cdc15239ed85e546b9dcdf7b7
moody2020 <mrmohammed98@mail.ru>
2016-07-10 17:01:09 +0300

    Update ar-TH3_BOSS1.lua

--- plugins/ar-TH3_BOSS1.lua
+++ plugins/ar-TH3_BOSS1.lua
@@ -2,7 +2,7 @@
 ▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
 ▀▄ ▄▀                                       ▀▄ ▄▀ 
 ▀▄ ▄▀       BY MOHAMMED HISHAM              ▀▄ ▄▀ 
-▀▄ ▄▀ BY MOHAMMEDHISHAM (@oOMOOMOo)         ▀▄ ▄▀          
+▀▄ ▄▀ BY MOHAMMEDHISHAM (@TH3BOSS)          ▀▄ ▄▀          
 ▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM        ▀▄ ▄▀   
 ▀▄ ▄▀          HELP1  : مساعدة              ▀▄ ▄▀ 
 ▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
@@ -63,7 +63,7 @@ local S = [[
  🔸➖🔹➖🔸➖🔹➖
 👇🏿تأبعـونأَ كل جَديد عل قنأةَ السورس👇🏿
                  [ @llDEV1ll ]
-👇🏿للأستفسار:- راسل المطور:- ☢⚜ 🔶 DEV:- @oOMOOMOo
+👇🏿للأستفسار:- راسل المطور:- ☢⚜ 🔶 DEV:- @TH3BOSS
 🔶 SUPPORT :- @ll60Kllbot
 ]]
 reply_msg(reply_id, S, ok_cb, false)

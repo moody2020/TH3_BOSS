58477f3bf0a8cf812541e28921c40c5bf3e7b657
moody2020 <mrmohammed98@mail.ru>
2016-07-14 09:50:59 +0300

    Update ar-help.lua

--- plugins/ar-help.lua
+++ plugins/ar-help.lua
@@ -1,3 +1,18 @@
+
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                       ▀▄ ▄▀ 
+▀▄ ▄▀       BY MOHAMMED HISHAM              ▀▄ ▄▀ 
+▀▄ ▄▀ BY MOHAMMEDHISHAM (@TH3BOSS)          ▀▄ ▄▀          
+▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM        ▀▄ ▄▀   
+▀▄ ▄▀          الاوامر▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+do
+
+function run(msg, matches)
+local reply_id = msg['id']
+local S = [[ 
 ⱲєℓϚ๏๓є ₮๏ Ͳћє ꝕᴙịⱴạτє ⱳ๑ᴙℓᴆ
 🃏〰〰〰〰〰〰〰〰〰🃏
 
@@ -17,3 +32,15 @@
 💯-Đєⱴ💀: @TH3BOSS
 💯-Đєⱴ ฿๏ͳ💀: @ll60Kllbot
 💯-Đєⱴ Ϲḫ₳ͷͷєℓ💀: @llDEV1ll
+reply_msg(reply_id, S, ok_cb, false)
+end
+
+return {
+description = "Help list", 
+usage = "Help list",
+patterns = {
+"^(الاوامر)$",
+},
+run = run 
+}
+end

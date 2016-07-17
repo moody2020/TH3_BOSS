5f5bbdc3262e837a56f82d34093bc3a2a3c0158c
moody2020 <mrmohammed98@mail.ru>
2016-07-17 09:36:17 +0300

    Add files via upload

---
+++ plugins/ar-TH3_BOSS7.lua
@@ -0,0 +1,43 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                       ▀▄ ▄▀ 
+▀▄ ▄▀       BY MOHAMMED HISHAM              ▀▄ ▄▀ 
+▀▄ ▄▀ BY MOHAMMEDHISHAM (@TH3BOSS)          ▀▄ ▄▀          
+▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM        ▀▄ ▄▀   
+▀▄ ▄▀          dev   : dev                  ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]] 
+do
+
+function run(msg, matches)
+return [[
+
+االبوت الذي يعمل على مجوعات السوبر 🔸
+
+يعمل البوت على مجموعات سوبر تصل الى5k عضو 🔷
+
+     ≪تم صنع البوت بواسطة المطور≫
+                      『  @TH3BOSS 』
+                      
+                      🔶DEV :🔶DEV : @TH3BOSS
+                      
+                      👇🏿تأبعـونأَ كل جَديد عل قنأةَ السورس👇🏿
+                      
+                      [ @llDEV1ll ]
+                      
+                     👇🏿للأستفسار:- راسل المطور:- ☢⚜ 🔶 DEV:- @H3BOSS
+                     
+                     🔶 SUPPORT :- @ll60Kllboto
+                      
+]]
+end
+
+return {
+description = "Shows bot q", 
+usage = "spam Shows bot q",
+patterns = {
+"^(المطور)$",
+},
+run = run 
+}
+end

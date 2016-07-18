82a7aa5595cc67a25d1f6ec41e9be083d856dc0f
moody2020 <mrmohammed98@mail.ru>
2016-07-18 17:19:09 +0300

    Update nedme.lua

--- plugins/nedme.lua
+++ plugins/nedme.lua
@@ -2,7 +2,7 @@ do
 
  function mohammed(msg, matches)
  local ch = '226861861'..msg.to.id--هنا حط ايديك
- local fuse = 'طلب احدهم مجموعه🌚\n\n❤️ايديه ' .. msg.from.id .. '\n\nاسمه❤️ ' .. msg.from.print_name ..'\n\nمعرفه❤️ @' .. msg.from.username ..'\n\nايدي المجموعه  ❤️ '..msg.to.id.. '\n\n❤️طلب منك؟\n' .. matches[1]
+ local fuse = 'طلب احدهم مجموعه🌚\n\n🔹️ايديه ' .. msg.from.id .. '\n\nاسمه🔹️ ' .. msg.from.print_name ..'\n\nمعرفه🔹️ @' .. msg.from.username ..'\n\nايدي المجموعه  🔹️ '..msg.to.id.. '\n\n🔹️طلب منك؟\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
 
 
@@ -10,8 +10,8 @@ do
    local chat = "user#id"..226861861--هنا حط ايديك
 
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'تم ارسال طلبك لصنع مجموعه الى المطور \n @th3_BOSS\nالرجاء انتضار المطور لصنع مجموعه لك عزيزي \n\n@'..msg.from.username
- end
+return 'تم ارسال طلبك لصنع مجموعه الى المطور \n @TH3BOSS\nالرجاء انتضار المطور لصنع مجموعه لك عزيزي \n\n@'..msg.from.username
+end
  end
  return {
 

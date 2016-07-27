3b7aea9792ee7da2630637e79e6739e6f9f5b4b0
moody2020 <mrmohammed98@mail.ru>
2016-07-27 14:19:06 +0000

    Update ar-TH3_BOSS.lua

--- plugins/ar-TH3_BOSS.lua
+++ plugins/ar-TH3_BOSS.lua
@@ -1,4 +1,4 @@
-  --[[ 
+--[[ 
   ▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
   ▀▄ ▄▀                                      ▀▄ ▄▀ 
   ▀▄ ▄▀    BY MOHAMMED HISHAM                ▀▄ ▄▀ 
@@ -12,7 +12,7 @@
   
   function mohammed(msg, matches)
   local reply_id = msg['id']
-    local S = '💢 السورس  TH3_BOSS 📁\n 💢   الاصدار 📋 V5\n 💢  الموقع \n \nhttps://github.com/moody2020/TH3_BOSS\n 💢    المطور : @TH3BOSS \n 💢   بوت_المطور :  @ll60Kllbot \n 💢   قناة_البوت :  @llDEV1ll '
+    local S = '💢 السورس  TH3_BOSS 📁\n 💢    الاصدار 📋 V5\n 💢   الموقع \n \nhttps://github.com/moody2020/TH3_BOSS\n 💢    المطور : @TH3BOSS \n 💢   بوت_المطور :  @ll60Kllbot \n 💢   قناة_البوت :  @llDEV1ll '
   reply_msg(reply_id, S, ok_cb, false)
   end
   

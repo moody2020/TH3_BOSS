630d25672f87ccb3857e664a40c80900bdb39a64
moody2020 <mrmohammed98@mail.ru>
2016-07-30 10:19:17 +0300

    Update banhammer.lua

--- plugins/banhammer.lua
+++ plugins/banhammer.lua
@@ -145,7 +145,7 @@ local function run(msg, matches)
 local support_id = msg.from.id
  if matches[1]:lower() == 'ايدي' and msg.to.type == "chat" or msg.to.type == "user" then
     if msg.to.type == "user" then
-      return "🔹   ايدي البوت : "..msg.to.id.. "\n\n🔹   ايدي حسابك : "..msg.from.id.. "\n\n🔹   المطور   @TH3BOSS "
+      return "💢    ايدي البوت : "..msg.to.id.. "\n\n💢    ايدي حسابك : "..msg.from.id.. "\n مـطـور الـسـورس\n الــــزعـــيـــــم > @TH3BOSS 💢 "
     end
     if type(msg.reply_id) ~= "nil" then
       local print_name = user_print_name(msg.from):gsub("‮", "")

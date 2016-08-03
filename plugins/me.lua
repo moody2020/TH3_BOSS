0bd6f0cc8d982e73e1d1c4810acff2e7ba3a5a87
moody2020 <mrmohammed98@mail.ru>
2016-08-03 11:10:33 +0000

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -13,19 +13,19 @@ local function run(msg, matches)
   if matches[1] == 'موقعي' then
     if is_sudo(msg) then
     send_document(get_receiver(msg), "./files/me/sudo.webp", ok_cb, false)
-      return 'انت المطور مالتي 😻🙊\n\n 💢  مـعـرفك :️ @'..msg.from.username
+      return "انت المطور مالتي 😻🙊"
     elseif is_admin1(msg) then
     send_document(get_receiver(msg), "./files/me/support.webp", ok_cb, false)
-      return 'انت اداري  🌚💭\n\n 💢  مـعـرفك :️ @'..msg.from.username
+      return "انت اداري  🌚💭"
     elseif is_owner(msg) then
     send_document(get_receiver(msg), "./files/me/owner.webp", ok_cb, false)
-      return 'انت مدير المجموعه 🌺😍\n\n 💢  مـعـرفك :️ @'..msg.from.username
+      return "انت مدير المجموعه 🌺😍"
     elseif is_momod(msg) then
     send_document(get_receiver(msg), "./files/me/moderator.webp", ok_cb, false)
-      return 'انت ادمن ☺️\n\n 💢  مـعـرفك :️ @'..msg.from.username
+      return "انت ادمن ☺️"
     else
     send_document(get_receiver(msg), "./files/me/member.webp", ok_cb, false)
-      return 'انت مجرد عضو 😒💔\n\n 💢  مـعـرفك :️ @'..msg.from.username
+      return "انت مجرد عضو 😒💔"
     end
   end
 end

acb0ac8e2908775d42638898ec31d37dbadbf581
moody2020 <mrmohammed98@mail.ru>
2016-08-07 09:55:41 +0300

    Update and rename plugins/TH3BOSSVIRSON.lua to VIRSON.lua

--- VIRSON.lua
+++ VIRSON.lua
@@ -12,7 +12,7 @@
   
   function mohammed(msg, matches)
   local reply_id = msg['id']
-    local S = ' 💢      السورس    TH3_BOSS 📁\n\n💢     الاصدار 📋 V5\n\n💢     الموقع \n \nhttps://github.com/moody2020/TH3_BOSS\n\n💢     المطور : @TH3BOSS \n\n💢     بـوت الـمـطـور  :  @ll60Kllbot \n \n💢    قناة_البوت :  @llDEV1ll '  reply_msg(reply_id, S, ok_cb, false)
+    local S = ' 🔺      السورس    TH3BOSS 📁\n\n🔺     الاصدار 📋 v 5 \n\n🔺     الموقع \n \nhttps://github.com/moody2020/TH3BOSS\n\n🔺     المطور : @TH3BOSS \n\n🔺     بـوت الـمـطـور  :  @ll60Kllbot \n \n🔺   قـنـاه الـسـورس :  @llDEV1ll '  reply_msg(reply_id, S, ok_cb, false)
   end
   
   return {

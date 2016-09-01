3e33cddb6e9506f67f40bf4179323d9330f0aa76
moody2020 <th3boss98@mail.ru>
2016-09-01 06:54:02 +0000

    Update lock_media.lua

--- plugins/lock_media.lua
+++ plugins/lock_media.lua
@@ -17,7 +17,7 @@ local chat = msg.to.id
     if redis:get(moody) and msg.media and not is_momod(msg) then 
 
             delete_msg(msg.id, ok_cb, false) 
-local test = "عزيزي~["..msg.from.first_name.."]".."\n".."يمنع نشر صور فيديوهات صوتيات وكافة الميديا هنا ان تكرر الامر سوف تجبرني على طردك😼☝️".."\n".." 🔺🔻 username : @"..(msg.from.username or " ") 
+local test = " 🗣 كبد عمري ["..msg.from.first_name.."]".."\n".."يمنع نشر صور فيديوهات صوتيات وكافة الميديا هنا ان تكرر الامر سوف تجبرني على طردك يرجى اتباع القوانين 😽☝️".."\n".." 👮 username : @"..(msg.from.username or " ") 
 reply_msg(mohammed, test, ok_cb, true) 
 
 end 
@@ -34,7 +34,7 @@ local mohammed = msg['id']
 local boss = '☑️ تم قفل 🔒 جميع الوسائط 🔕 \n🔺Order By : @'..msg.from.username..'\n🔻Order By : '.. msg.from.id..'\n'
 reply_msg(mohammed, boss, ok_cb, true) 
 elseif matches[1] == 'قفل الوسائط' and not is_momod(msg) then 
-local moody = 'للمشرفين فقط 🔺🔻'
+local moody = 'للـمـشـرفـيـن فـقـط 👮🖕🏿'
 reply_msg(mohammed, moody, ok_cb, true) 
   elseif is_momod(msg) and matches[1] == 'فتح الوسائط' then
       local th3boss= 'mate:'..msg.to.id
@@ -42,66 +42,7 @@ reply_msg(mohammed, moody, ok_cb, true)
 local boss = '☑️ تم فتح جميع الوسائط 🔓🔔 \n🔺Order By : @'..msg.from.username..'\n🔻Order By : '.. msg.from.id..'\n'
 reply_msg(mohammed, boss, ok_cb, true) 
 elseif matches[1] == 'فتح الوسائط' and not is_momod(msg) then 
-local moody= 'للمشرفين فقط 🔺🔻'
-reply_msg(mohammed, moody, ok_cb, true) 
-end 
-end 
-
-return { 
-    patterns = { 
-    "^(قفل الوسائط)$", 
-    "^(فتح الوسائط)$", 
-  },
-run = MOHAMMED, 
-    pre_process = pre_process 
-} 
-
-end
---[[ 
-▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄ ▄▀    BY MOHAMMED HISHAM                ▀▄ ▄▀ 
-▀▄ ▄▀   BY MOHAMMEDHISHAM (@TH3BOSS)      ▀▄ ▄▀ 
-▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM       ▀▄ ▄▀   
-▀▄ ▄▀                   كتم الوسائط            ▀▄ ▄▀ 
-▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
-—]]
-do 
-
-local function pre_process(msg) 
-local mohammed = msg['id'] 
-  local user = msg.from.id 
-local chat = msg.to.id 
-    local moody = 'mate:'..msg.to.id 
-    if redis:get(moody) and msg.media and not is_momod(msg) then 
-
-            delete_msg(msg.id, ok_cb, false) 
-local test = "عزيزي~["..msg.from.first_name.."]".."\n".."يمنع نشر صور فيديوهات صوتيات وكافة الميديا هنا ان تكرر الامر سوف تجبرني على طردك😼☝️".."\n".." 🔺🔻 username : @"..(msg.from.username or " ") 
-reply_msg(mohammed, test, ok_cb, true) 
-
-end 
-
-        return msg 
-    end 
-
-local function MOHAMMED(msg, matches) 
-local mohammed = msg['id'] 
-
-    if matches[1] == 'قفل الوسائط'  and is_momod(msg) then 
-                    local th3boss= 'mate:'..msg.to.id 
-                    redis:set(th3boss, true) 
-local boss = '☑️ تم قفل 🔒 جميع الوسائط 🔕 \n🔺Order By : @'..msg.from.username..'\n🔻Order By : '.. msg.from.id..'\n'
-reply_msg(mohammed, boss, ok_cb, true) 
-elseif matches[1] == 'قفل الوسائط' and not is_momod(msg) then 
-local moody = 'للمشرفين فقط 🔺🔻'
-reply_msg(mohammed, moody, ok_cb, true) 
-  elseif is_momod(msg) and matches[1] == 'فتح الوسائط' then
-      local th3boss= 'mate:'..msg.to.id
-      redis:del(th3boss)
-local boss = '☑️ تم فتح جميع الوسائط 🔓🔔 \n🔺Order By : @'..msg.from.username..'\n🔻Order By : '.. msg.from.id..'\n'
-reply_msg(mohammed, boss, ok_cb, true) 
-elseif matches[1] == 'فتح الوسائط' and not is_momod(msg) then 
-local moody= 'للمشرفين فقط 🔺🔻'
+local moody= 'للـمـشـرفـيـن فـقـط 👮🖕🏿'
 reply_msg(mohammed, moody, ok_cb, true) 
 end 
 end 

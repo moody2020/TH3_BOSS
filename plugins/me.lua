166cd1339f3efd15b765e35c8e3ef46c8aac62cd
moody2020 <th3boss98@mail.ru>
2016-09-01 07:11:43 +0000

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -13,39 +13,39 @@ local function mohammedboss(msg, matches)
   if matches[1] == 'موقعي' then
     if is_sudo(msg) then
     send_document(get_receiver(msg), "./files/me/sudo.webp", ok_cb, false)
-      return "🔺انت المطور مالتي 😻🙊\n🔺 اسمك :"..msg.from.first_name.."\n" 
-   .."🔺ايديك :("..msg.from.id..")\n" 
-   .."🔺ايدي الكروب :("..msg.to.id..")\n" 
-   .."🔺معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
-   .."🔺رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
+      return "🚩انت المطور مالتي 😻🙊\n🚩 اسمك :"..msg.from.first_name.."\n" 
+   .."🚩ايديك :("..msg.from.id..")\n" 
+   .."🚩ايدي الكروب :("..msg.to.id..")\n" 
+   .."🚩معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."🚩رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
     elseif is_admin1(msg) then
     send_document(get_receiver(msg), "./files/me/support.webp", ok_cb, false)
-      return "🔺انت اداري  🌚💭\n🔺 اسمك :"..msg.from.first_name.."\n" 
-   .."🔺ايديك :("..msg.from.id..")\n" 
-   .."🔺ايدي الكروب :("..msg.to.id..")\n" 
-   .."🔺معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
-   .."🔺رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
+      return "🚩انت اداري  🌚💭\n🚩اسمك :"..msg.from.first_name.."\n" 
+   .."🚩ايديك :("..msg.from.id..")\n" 
+   .."🚩ايدي الكروب :("..msg.to.id..")\n" 
+   .."🚩معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."🚩رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
     elseif is_owner(msg) then
     send_document(get_receiver(msg), "./files/me/owner.webp", ok_cb, false)
-      return "🔺انت مدير المجموعه 🌺😍\n🔺 اسمك :"..msg.from.first_name.."\n" 
-   .."🔺ايديك :("..msg.from.id..")\n" 
-   .."🔺ايدي الكروب :("..msg.to.id..")\n" 
-   .."🔺معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
-   .."🔺رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
+      return "🚩انت مدير المجموعه 🌺😍\n🚩 اسمك :"..msg.from.first_name.."\n" 
+   .."🚩ايديك :("..msg.from.id..")\n" 
+   .."🚩ايدي الكروب :("..msg.to.id..")\n" 
+   .."🚩معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."🚩رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
     elseif is_momod(msg) then
     send_document(get_receiver(msg), "./files/me/moderator.webp", ok_cb, false)
-      return "🔺انت ادمن ☺️\n🔺 اسمك :"..msg.from.first_name.."\n" 
-   .."🔺ايديك :("..msg.from.id..")\n" 
-   .."🔺ايدي الكروب :("..msg.to.id..")\n" 
-   .."🔺معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
-   .."🔺رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
+      return "🚩انت ادمن ☺️\n🚩 اسمك :"..msg.from.first_name.."\n" 
+   .."🚩ايديك :("..msg.from.id..")\n" 
+   .."🚩ايدي الكروب :("..msg.to.id..")\n" 
+   .."🚩معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."🚩رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
           else
     send_document(get_receiver(msg), "./files/me/member.webp", ok_cb, false)
-      return "🔺انت مجرد عضو 😒💔\n🔺 اسمك :"..msg.from.first_name.."\n" 
-   .."🔺ايديك :("..msg.from.id..")\n" 
-   .."🔺ايدي الكروب :("..msg.to.id..")\n" 
-   .."🔺معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
-   .."🔺رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
+      return "🚩انت مجرد عضو 😒💔\n🚩 اسمك :"..msg.from.first_name.."\n" 
+   .."🚩ايديك :("..msg.from.id..")\n" 
+   .."🚩ايدي الكروب :("..msg.to.id..")\n" 
+   .."🚩معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."🚩رقمك :("..(msg.from.phone or "  لا يوجد ")..")\n"
     end
   end
 end

958a88517aed0cf400c2f63d43e359d5bde0805a
moody2020 <th3boss98@mail.ru>
2016-09-01 12:17:03 +0000

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,5 +1,5 @@
 do local _ = {
-  about_text = "🔺Welcome to TH3BOSS v5 For more information Subscribe to the channel @llDEV1lln\n https://github.com/moody2020/TH3BOSS\n\n🔺Dev @TH3BOSS\n\n🔺 Dev Bot @ll60Kllbot\n\n🔺Dev channel @llDEV1ll",
+  about_text = "🚩Welcome to TH3BOSS V6 For more information Subscribe to the channel @llDEV1lln\n https://github.com/moody2020/TH3BOSS\n\n🚩Dev @TH3BOSS\n\n🚩 Dev Bot @ll60Kllbot\n\n 🚩channel @llDEV1ll",
   enabled_plugins = {
      "badword",
      "admin",

44bafd7c40729d66291f451b57fde4b13c30426b
moody2020 <th3boss98@mail.ru>
2016-09-03 13:14:14 +0000

    Update hello.lua

--- plugins/hello.lua
+++ plugins/hello.lua
@@ -1,11 +1,11 @@
 do
     
-local function th3boss(msg,matches)
+local function mohammedboss(msg,matches)
     if matches[1] == "chat_add_user"  then 
-      return "⚜ اهـلا بـك گــ💔ـبــد ❤» ؏ـٍْمريـﮧ ⚜ \n".."🔹 اسم المجموعة :: "..msg.to.title.."\n".." 🔸ايدي المجموعة :: "..msg.to.id.."\n".."🔹 اسم الضافك ::"..(msg.from.first_name or " ").."\n".."🔹 معرف الضافك :: @"..(msg.from.username or " ").."\n".."🔸 معرفك :: @"..(msg.action.user.username or "لايوجد " ).."\n".."🔸 ايدي الضافك ::"..msg.from.id
+      return "⚜ اهـلا بـك عـزيـزي ❤️ \n".."▫️ اسم المجموعة :: "..msg.to.title.."\n".." ▫️ايدي المجموعة :: "..msg.to.id.."\n".."▫️ اسم الضافك ::"..(msg.from.first_name or " ").."\n".."▫️ معرف الضافك :: @"..(msg.from.username or " ").."\n".."▫️ معرفك :: @"..(msg.action.user.username or "لايوجد " ).."\n".."▫️ ايدي الضافك ::"..msg.from.id
 
     elseif matches[1] == "chat_add_user_link" then
-      return "⚜ اهـلا بـك گــ💔ـبــد ❤» ؏ـٍْمريـﮧ ⚜  \n".."🔹 اسم المجموعة :: "..msg.to.title.."\n".."🔸 ايدي المجموعة :: "..msg.to.id.."\n".."🔹 اسم الضافك :: "..(msg.from.first_name or " ").."\n".."🔸 معرف الضافك :: @"..(msg.from.username or " ").."\n".."🔹 معرفك :: @"..(msg.from.username or "لايوجد " ).."\n".." 🔸ايديك ::"..msg.from.id
+      return "⚜ اهـلا بـك عـزيـزي ❤️\n".."▫️ اسم المجموعة :: "..msg.to.title.."\n".."▫️ ايدي المجموعة :: "..msg.to.id.."\n".."▫️ اسم الضافك :: "..(msg.from.first_name or " ").."\n".."▫️ معرف الي ضافك :: @"..(msg.from.username or " ").."\n".."▫️ معرفك :: @"..(msg.from.username or "لايوجد " ).."\n".." ▫️ايديك ::"..msg.from.id
 
     end
     if matches[1] == "chat_del_user" then
@@ -19,6 +19,6 @@ return {
         "^!!tgservice (chat_del_user)$"
        
     },
- run = th3boss,
+ run = mohammedboss,
 }
 end

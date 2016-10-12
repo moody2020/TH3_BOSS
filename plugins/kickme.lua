eeeb2507d38c88e2670abdae2a478d75eb5d5709
moody2020 <th3boss98@mail.ru>
2016-10-12 10:45:23 +0000

    Update kickme.lua

--- plugins/kickme.lua
+++ plugins/kickme.lua
@@ -12,7 +12,7 @@ local function run(msg, matches)
 if matches[1] == 'اطردني' then 
 local hash = 'kick:'..msg.to.id..':'..msg.from.id 
      redis:set(hash, "waite") 
-      return 'عزيزي ❗️ ('..msg.from.username..')\nهل انت متاكد من الخروج من المجموعة ارسل (نعم)✔️ لكي تخرج من المجموعة ان لم تكن متأكد من الخروج ارسل (لا) ✖️' 
+      return 'عزيزي ❗️ (@'..msg.from.username..')\nهل انت متاكد من الخروج من المجموعة ارسل (نعم)☑️ لكي تخرج من المجموعة ان لم تكن متأكد من الخروج ارسل (لا) ✖️' 
     end 
 
     if msg.text then 
@@ -20,7 +20,7 @@ local hash = 'kick:'..msg.to.id..':'..msg.from.id
       if msg.text:match("^نعم$") and redis:get(hash) == "waite" then 
      redis:set(hash, "ok") 
    elseif msg.text:match("^لا$") and redis:get(hash) == "waite" then 
-   send_large_msg(get_receiver(msg), "تم الغاء طردك من المجموعة 📛") 
+   send_large_msg(get_receiver(msg), "تم الغاء طردك من المجموعة 🌐") 
      redis:del(hash, true) 
 
       end 

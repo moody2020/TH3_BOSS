9724898d80625d5eb9c17e2416dda5736acf37bf
moody2020 <th3boss98@mail.ru>
2016-11-24 11:32:24 +0000

    Update lock_fwd.lua

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -15,7 +15,65 @@ local function pre_process(msg)
     if redis:get(fwd) and not is_momod(msg) and msg.fwd_from then
             delete_msg(msg.id, ok_cb, true)
             return msg
-end
+end--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀ 
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY MOHAMMED HISHAM                ▀▄ ▄▀ 
+▀▄ ▄▀   BY MOHAMMEDHISHAM (@TH3BOSS)      ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM       ▀▄ ▄▀ 
+▀▄ ▄▀   ANTI FWD  :  منع اعاده توجيه           ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀ 
+--]] 
+
+do 
+local function pre_process(msg) 
+
+    local fwd = 'mate:'..msg.to.id 
+    if redis:get(fwd) and not is_momod(msg) and msg.fwd_from then 
+            delete_msg(msg.id, ok_cb, true) 
+            return msg 
+end 
+        return msg 
+    end 
+ local function mohammed(msg, matches) 
+     chat_id = msg.to.id 
+local reply_id = msg['id'] 
+     if is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then 
+         local fwd = 'mate:'..msg.to.id 
+         redis:set(fwd, true) 
+         local text = 'تم ☑️ قفل 🔒 اعاده توجيه ✋😽\n📌 Order By : @'..msg.from.username..'\n📌 Order By : '.. msg.from.id..'\n'
+         return reply_msg(reply_id, text, ok_cb, false) 
+         end 
+local reply_id = msg['id'] 
+    if not is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then 
+    local text= 'للـمـشـرفـيـن فـقـط 👮🖕🏿' 
+ return reply_msg(reply_id, text, ok_cb, false) 
+end 
+local reply_id = msg['id'] 
+if is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then 
+    local fwd = 'mate:'..msg.to.id 
+    redis:del(fwd) 
+    local text = 'تم ☑️ فتح 🔓 اعاده توجيه ✋😽\n📌 Order By : @'..msg.from.username..'\n📌 Order By : '.. msg.from.id..'\n'
+    return reply_msg(reply_id, text, ok_cb, false) 
+end 
+
+local reply_id = msg['id'] 
+if not is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then 
+local text= ' للـمـشـرفـيـن فـقـط 👮🖕🏿' 
+ return reply_msg(reply_id, text, ok_cb, false) 
+ end 
+
+end 
+return { 
+    patterns ={ 
+        '^(قفل) (اعاده توجيه)$', 
+        '^(فتح) (اعاده توجيه)$' 
+    }, 
+run = mohammed, 
+pre_process = pre_process 
+}
+end 
+
         return msg
     end
     

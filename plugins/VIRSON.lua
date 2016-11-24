99034da824a342519c163b2f60e0102092262b72
moody2020 <th3boss98@mail.ru>
2016-11-24 12:22:37 +0000

    Update VIRSON.lua

--- plugins/VIRSON.lua
+++ plugins/VIRSON.lua
@@ -1,25 +1,21 @@
 --[[ 
-  ▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+  ▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀ 
   ▀▄ ▄▀                                      ▀▄ ▄▀ 
   ▀▄ ▄▀    BY MOHAMMED HISHAM                ▀▄ ▄▀ 
   ▀▄ ▄▀     BY MOHAMMEDHISHAM (@TH3BOSS)     ▀▄ ▄▀ 
-  ▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM       ▀▄ ▄▀   
+  ▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM       ▀▄ ▄▀ 
   ▀▄ ▄▀      VIRSON      :  الاصدار           ▀▄ ▄▀ 
-  ▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
-  --]]
-  
-  do
-  
-  function mohammed(msg, matches)
-  local reply_id = msg['id']
-    local S = ' ▫️     السورس    TH3BOSS 📁\n\n▫️     الاصدار 📋 V7  \n\n▫️     الموقع \n \nhttps://github.com/moody2020/TH3BOSS\n\n▫️     المطور : @TH3BOSS \n\n▫️     بـوت الـمـطـور  :  @ll60Kllbot \n \n▫️   قـنـاه الـسـورس :  @llDEV1ll '  reply_msg(reply_id, S, ok_cb, false)
-  end
-  
-  return {
-    patterns = {
-  "^(الاصدار)$",
+  ▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀ 
+  --]] 
+  do 
+  function mohammed(msg, matches) 
+  local reply_id = msg['id'] 
+    local S = ' ▫️     السورس    TH3BOSS 📁\n\n▫️     الاصدار 📋 V8  \n\n▫️     الموقع \n \nhttps://github.com/moody2020/TH3BOSS\n\n▫️     المطور : @TH3BOSS \n\n▫️     بـوت الـمـطـور  :  @ll60Kllbot \n \n▫️   قـنـاه الـسـورس :  @llDEV1ll '  reply_msg(reply_id, S, ok_cb, false) 
+  end 
+  return { 
+    patterns = { 
+  "^(الاصدار)$", 
     }, 
     run = mohammed 
-  }
-  
+  } 
   end

d9298a49ec9ee712cb1d59bad4c4f06158c10875
moody2020 <th3boss98@mail.ru>
2016-12-23 11:49:30 +0300

    Update VIRSON.lua

--- plugins/VIRSON.lua
+++ plugins/VIRSON.lua
@@ -10,7 +10,7 @@
   do 
   function mohammed(msg, matches) 
   local reply_id = msg['id'] 
-    local S = ' ▫️     السورس    TH3BOSS 📁\n\n▫️     الاصدار 📋 V8  \n\n▫️     الموقع \n \nhttps://github.com/moody2020/TH3BOSS\n\n▫️     المطور : @TH3BOSS \n\n▫️     بـوت الـمـطـور  :  @ll60Kllbot \n \n▫️   قـنـاه الـسـورس :  @llDEV1ll '  reply_msg(reply_id, S, ok_cb, false) 
+    local S = ' ▫️     السورس    TH3BOSS 📁\n\n▫️     الاصدار 📋 V10  \n\n▫️     الموقع \n \nhttps://github.com/moody2020/TH3BOSS\n\n▫️     المطور : @TH3BOSS \n\n▫️     بـوت الـمـطـور  :  @ll60Kllbot \n \n▫️   قـنـاه الـسـورس :  @llDEV1ll '  reply_msg(reply_id, S, ok_cb, false) 
   end 
   return { 
     patterns = { 
